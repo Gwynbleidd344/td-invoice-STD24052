29c120cd5d0a73d4dbb147d729e71cf1d34404a6
Vaniaina <vanimendes344@gmail.com>
2026-02-18 16:19:46 +0300

    feat add question 2 method

--- src/main/java/DataRetriever.java
+++ src/main/java/DataRetriever.java
@@ -32,4 +32,32 @@ public class DataRetriever {
         }
     }
 
+    public List<InvoiceTotal> findConfirmedAndPaidInvoiceTotals() {
+        DBConnection conn = new DBConnection();
+            String sql = """
+                        SELECT i.id, i.customer_name, i.status, SUM(il.quantity * il.unit_price) as total
+                    FROM invoice i
+                    JOIN invoice_line il ON i.id = il.invoice_id
+                    WHERE i.status IN ('CONFIRMED', 'PAID')
+                    GROUP BY i.id, i.customer_name, i.status
+                    ORDER BY i.id;
+                        """;
+        List<InvoiceTotal> invoiceTotals = new ArrayList<>();
+        try {
+            PreparedStatement ps = conn.getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                InvoiceTotal invoiceTotal = new InvoiceTotal(
+                    rs.getInt("id"),
+                    rs.getString("customer_name"),
+                    InvoiceStatus.valueOf(rs.getString("status")),
+                    rs.getDouble("total")
+                );
+                invoiceTotals.add(invoiceTotal);
+            }
+            return invoiceTotals;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         DataRetriever dataRetriever = new DataRetriever();
         System.out.println(dataRetriever.findInvoiceTotals());
+        System.out.println(dataRetriever.findConfirmedAndPaidInvoiceTotals());
     }
 }
